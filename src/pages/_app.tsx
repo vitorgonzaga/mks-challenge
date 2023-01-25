af8e41ebec4625665c04de340c0cfb372e9ed9df
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-25 14:28:51 -0300

    feat: add Provider from react-redux wrapping _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from '../store/store'
 import { theme } from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} >
-      <Component {...pageProps} />
+      <Provider store={store} >
+        <Component {...pageProps} />
+      </Provider>
     </ChakraProvider>
   )
 }
