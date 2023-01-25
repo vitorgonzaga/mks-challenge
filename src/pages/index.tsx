7cad531942376d9f066648920ec1c980a3bd375e
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-25 09:41:05 -0300

    chore: displaying Product Cards in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,40 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { ProductCard } from "@/components/ProductCard";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <h1>mks sistemas</h1>
+    <Box
+      w='100vw'
+      h='100vh'
+    >
+      <Header />
+      <Flex
+        pt='101px'
+        h='calc(100vh - 34px)'
+        align='center'
+        justify='center'
+      >
+        <Flex
+          w='938px'
+          maxWidth={950}
+          h='100%'
+          maxHeight={630}
+          wrap='wrap'
+          justify='space-between'
+        >
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </Flex>
+      </Flex>
+      <Footer />
+    </Box>
   )
 }
