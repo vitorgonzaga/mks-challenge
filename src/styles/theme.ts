ac79a9dea9e8765394a9ecaea044bf5517b294d9
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-25 09:38:55 -0300

    chore: add chakra provider in _app and create theme.ts customized

---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    transparent: 'transparent',
+    black: '#000',
+    white: '#fff',
+    brand: {
+      blue: {
+        500: '#0F52BA'
+      },
+      gray: {
+        100: '#E5E5E5',
+        200: '#EEEEEE',
+        500: '#373737'
+      },
+      font: {
+        gray: {
+          500: '#2C2C2C'
+        }
+      }
+    }
+  },
+  fonts: {
+    body: 'Montserrat',
+    heading: 'Montserrat'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'brand.gray.100',
+      },
+      a: {
+        color: 'inherit',
+        textDecoration: 'none'
+      },
+      // p: {
+      //   marginBlockStart: '0px',
+      //   marginBlockEnd: '0px'
+      // }
+    }
+  }
+})
