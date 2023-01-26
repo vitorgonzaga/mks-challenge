4cd60fa0b83a7899a2f67a10374ac688be6d3cd2
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-26 16:47:46 -0300

    feat(shoppingCartSlice): add saveCart, addProduct, incrementProduct and decrementProduct actions inside reducer.
    Also renamed selectIsOpen selector to "IsOpenSelector".
    
    Add cartLength and cart selectors.

--- src/store/shoppingCartSlice.ts
+++ src/store/shoppingCartSlice.ts
@@ -1,10 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
 import type { RootState } from './store'
+
+type ProductInCart = {
+  id: number,
+  photo: string,
+  name: string,
+  price: string,
+  amount: number
+}
 
 export const shoppingCartSlice = createSlice({
   name: 'shoppingCart',
   initialState: {
-    // products: <product[]>[],
+    cart: <ProductInCart[]>[],
     isOpen: false
   },
   reducers: {
@@ -13,13 +22,56 @@ export const shoppingCartSlice = createSlice({
     },
     onClose: (state) => {
       state.isOpen = false
+    },
+    saveCart: (state, action) => {
+      state.cart = action.payload
+    },
+    addProduct: (state, action) => {
+      const productExists = state.cart.find(product => product.id === action.payload.id)
+      if(productExists) {
+        toast.success('Esse produto já existe no seu carrinho!')
+      } else {
+        const newProduct = {
+          ...action.payload,
+          amount: 1
+        }
+        state.cart.push(newProduct)
+        localStorage.setItem('@mks:cart', JSON.stringify(state.cart))
+      }
+    },
+    incrementProduct: (state, action) => {
+      const productInCart = state.cart.find(product => product.id === action.payload.id)
+      if(productInCart) productInCart!.amount += 1
+    },
+    decrementProduct: (state, action) => {
+      const productInCart = state.cart.find(product => product.id === action.payload.id)
+      if (productInCart) {
+        if(productInCart.amount === 1) {
+          state.cart.filter(elem => {
+            elem.id != action.payload.id
+          })
+        } else {
+          productInCart.amount -= 1
+        }
+      }
+      localStorage.setItem('@mks:cart', JSON.stringify(state.cart))
     }
   }
 })
 
-export const { onOpen, onClose } = shoppingCartSlice.actions
 
-export const selectIsOpen = (state: RootState) => state.shoppingCart.isOpen
+export const {
+  onOpen,
+  onClose,
+  saveCart,
+  addProduct,
+  incrementProduct,
+  decrementProduct
+} = shoppingCartSlice.actions
+
+export const IsOpenSelector = (state: RootState) => state.shoppingCart.isOpen
+export const cartLength = (state: RootState) => state.shoppingCart.cart.length
+export const cart = (state: RootState) => state.shoppingCart.cart
 
 // the outside "thunk creator" function
 // export const fetchUserById = (userId: number) => {
