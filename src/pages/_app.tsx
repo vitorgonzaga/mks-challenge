5135ba7d285fb82db7062352ae0ef0ab35be251c
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-01-26 10:01:26 -0300

    chore(_app): add QueryClientProvider and ReactQueryDevtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
+import { queryClient } from '@/services/queryClient'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 import { theme } from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme} >
-      <Provider store={store} >
-        <Component {...pageProps} />
-      </Provider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient} >
+      <ChakraProvider theme={theme} >
+        <Provider store={store} >
+          <Component {...pageProps} />
+        </Provider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools  position='bottom-left' />
+    </QueryClientProvider>
   )
 }
